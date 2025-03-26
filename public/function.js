152a475584850b0448ca3d65375d60a450e791e7
THePila <joshua.pilaszek@gmail.com>
2025-03-26 13:46:04 -0300

    avances con los selectores del colorPicker

--- public/function.js
+++ public/function.js
@@ -6,6 +6,7 @@ $(window).on('load', function () {
 
 $(document).on('input', '#baseColorHex', reflejarHexAPaleta);
 $(document).on('change', '#selectColor', desplegarCursores);
+$(document).on('click', '#selectColor', generarPaleta);
 // $(document).on('mouseover', '.color-box', desplegarCursores);
 
 var colorPicker = new iro.ColorPicker("#pickerColor", {
@@ -62,19 +63,46 @@ function actualizarColorComplementario(interactuarConPrimero) {
     }
 }
 
-function actualizarColorMonocromatico() {
+// function actualizarColorMonocromatico() {
+//     const color1 = colorPicker.colors[0];
+//     const escalaDeGrises = [];
+
+//     // Generar una escala de grises basada en el color1
+//     for (let i = 0; i <= 10; i++) {
+//         const valorGris = i * 10; // Incrementar el valor para crear diferentes tonos de gris
+//         const colorGris = new iro.Color({ h: color1.hue, s: 0, v: valorGris });
+//         escalaDeGrises.push(colorGris);
+//     }
+
+//     // Aplicar la escala de grises al color picker
+//     for (let i = 0; i < escalaDeGrises.length; i++) {
+//         if (colorPicker.colors[i]) {
+//             colorPicker.colors[i].set(escalaDeGrises[i]);
+//         } else {
+//             colorPicker.colors.push(escalaDeGrises[i]);
+//         }
+//     }
+// }
+
+function actualizarColorAnalogico() {
     const color1 = colorPicker.colors[0];
     const color2 = colorPicker.colors[1];
-    const colorMonocromatico = new iro.Color({ h: color1.hue, s: color1.saturation, v: color1.value });
-    colorPicker.colors[1].set(colorMonocromatico);
+    const color3 = colorPicker.colors[2];
+
+    const color1Hue = color1.hue;
+    const color2Hue = color2.hue;
+    const color3Hue = color3.hue;
+    
+    console.log(color3Hue);
+
+    const color1HueOffset = (color1Hue + 30) % 360;
+    const color2HueOffset = (color2Hue + 30) % 360;
+    const color3HueOffset = (color3Hue + 30) % 360;
+
+    // colorPicker.colors[0].set(color1HueOffset);
+    // colorPicker.colors[1].set(color2HueOffset);
+    // colorPicker.colors[2].set(color3HueOffset);
 }
-
-// function actualizarColorAnalogico() {
-//     const color1 = colorPicker.colors[0];
-//     const color2 = colorPicker.colors[1];
-
-//     const colorAnalogico = new iro.Color({ h: color1.hue + 30, s: color1.saturation, v: color1.value });
-// }
 
 
 
@@ -89,11 +117,15 @@ function desplegarCursores() {
         colorPicker.on('color:change', function (color) {
             actualizarColorAnalogico();
         });
+        colorPicker.addColor("#f00");
     } else if (colorSelect.value === "complementario") {
         colorPicker.on('color:change', function (color) {
             const interactuarConPrimero = color.index === 0;
             actualizarColorComplementario(interactuarConPrimero);
         });
+        if (colorPicker.colors.length > 2) {
+            colorPicker.removeColor(2);
+        }
     }
 }
 
@@ -110,28 +142,38 @@ function convertirHSVAHex() {
 
 function generarPaleta(color1, color2) {
     $('#divPaleta').empty();
-    const selectColor = document.getElementById('selectColor');
-    if (selectColor == "complementario") {
-        const colorBox1 = `
+    const colorBox1 = `
     <div class="col color-box" style="background-color:${color1}">
         <button type="button" class="btn" style="backgroud-color:${color1}"><span>${color1}</span></button>
     </div>`;
-        const colorBox2 = `
+    const colorBox2 = `
     <div class="col color-box" style="background-color:${color2}">
         <button type="button" class="btn" style="backgroud-color:${color2}"><span>${color2}</span></button>
     </div>`;
-    }
-    else if (selectColor == "monocromatico") {
-        const colorBox1 = `
-    <div class="col color-box" style="background-color:${color1}">
-        <button type="button" class="btn" style="backgroud-color:${color1}"><span>${color1}</span></button>
-    </div>`;
-        const colorBox2 = `
-    <div class="col color-box" style="background-color:${color2}">
-        <button type="button" class="btn" style="backgroud-color:${color2}"><span>${color2}</span></button>
-    </div>`;
-    }
     $('#divPaleta').append(colorBox1, colorBox2);
+    // const selectColor = document.getElementById('selectColor');
+    // if (selectColor == "complementario") {
+    //     const colorBox1 = `
+    // <div class="col color-box" style="background-color:${color1}">
+    //     <button type="button" class="btn" style="backgroud-color:${color1}"><span>${color1}</span></button>
+    // </div>`;
+    //     const colorBox2 = `
+    // <div class="col color-box" style="background-color:${color2}">
+    //     <button type="button" class="btn" style="backgroud-color:${color2}"><span>${color2}</span></button>
+    // </div>`;
+    // $('#divPaleta').append(colorBox1, colorBox2);
+    // }
+    // else if (selectColor == "monocromatico") {
+    //     const colorBox1 = `
+    // <div class="col color-box" style="background-color:${color1}">
+    //     <button type="button" class="btn" style="backgroud-color:${color1}"><span>${color1}</span></button>
+    // </div>`;
+    //     const colorBox2 = `
+    // <div class="col color-box" style="background-color:${color2}">
+    //     <button type="button" class="btn" style="backgroud-color:${color2}"><span>${color2}</span></button>
+    // </div>`;
+    // $('#divPaleta').append(colorBox1, colorBox2);
+    // }
 }
 
 
